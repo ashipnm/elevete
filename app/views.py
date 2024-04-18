a138c19c5781384e194c8e24a4aab0bf1e95e152
ashipnm <muhammadashique352@gmail.com>
2024-04-18 15:12:34 +0530

    templates loaded

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django import forms
 from .models import *
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+
 
 
 def signup(request):
@@ -49,4 +52,84 @@ def index(request):
     user = request.user
     print(user)
     return render(request , 'index.html')
+def base(request):
+    return render(request,'base.html')
+def about(request):
+    return render(request,'about.html')
+def blog(request):
+    return render(request,'blog.html')
+def contact(request):
+    return render(request,'contact.html')
+def forgotpassword(request):
+    return render(request,'forgotpassword.html')
+def signout(request):
+    return render(request,'signout.html')
+def confirmation(request):
+    return render(request,'confirmation.html')
+def dashboard(request):
+    user = request.user   
+    if user.is_authenticated:
+        user = CustomUser.objects.get(email = user)
+        print(user.firstname)
+    else:
+        print("User is not authenticated")
+    return render(request,'dashboard.html', { 'user': user })
+
+@login_required
+def profiledeatils(request):
+    user = request.user   
+    
+    return render(request,'profiledetails.html' , { 'user': user })
+
+
+def address(request):
+    return render(request,'address.html')
+
+def shop(request, category):
+    # products = Product.objects.filter(category__name=category)
+    return render(request, 'shop.html')
+def signout(request):
+    return render(request,'signout.html')
+def product_details(request, product_id):
+    # product = get_object_or_404(Product, pk=product_id)
+    # product_images = [product.product_image_1, product.product_image_2, product.product_image_3, product.product_image_4, product.product_image_5]
+    # product_colours = product.Colors.all()
+    # product_size = product.sizes.all()
+    
+    
+    return render(request, 'product_details.html')
+
+
+
+def add_to_cart(request, product_id):
+    user = request.user
+    # cart_item = CartItem.objects.get(user = user)
+    # product = get_object_or_404(Product, pk=product_id)
+    # CartItem.objects.create(user=user ,
+    #                         product_name = product.product_name ,
+    #                         price = product.product_price ,
+    #                         quantity = 1,
+    #                         total = product.product_price)
+    
+
+    
+    
+    return redirect('view_cart')
+@login_required
+def view_cart(request):
+    # Retrieve cart items for the current user
+    # cart_items = CartItem.objects.filter(user=request.user)
+    
+    # total_price = sum(item.total for item in cart_items)
+    
+    return render(request, 'shop_cart.html')
+
+def remove_from_cart(request, product_id):
+    # Your logic to remove an item from the cart
+    return redirect('view_cart')
+
+
+
+
+
 
--- elevate/urls.py
+++ elevate/urls.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import *
-
+from app import views
+from django.conf.urls.static import static 
+from django.conf import settings 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup' , signup , name='signup'),
-    path('login/', user_login, name='login'),
-    path('index/' , index , name='index'),
+    path('home/',views.index,name='index'),
+    path('base/',views.base,name='base'),
+    path('about/',views.about,name='about'),
+    path('blog/',views.blog,name='blog'),
+    path('',views.user_login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('contact/',views.contact,name='contact'),
+    path('forgotpassword/',views.forgotpassword,name='forgotpassword'),
+    path('signout/',views.signout,name='signout'),
+    path('confirmation/',views.confirmation,name='confirmation'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('profiledetails/',views.profiledeatils,name='profiledetails'),
+    path('address/',views.address,name='address'),
+    path('shop/<str:category>/',views.shop,name='shop'),
+    path('signout/',views.signout,name="signout"),
+    path('product/<int:product_id>/', views.product_details, name='product_details'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
