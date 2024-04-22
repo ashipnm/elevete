49caed12e45e77925521fac14b166c564d22bf82
ashipnm <muhammadashique352@gmail.com>
2024-04-22 10:44:26 +0530

    products added

--- app/models.py
+++ app/models.py
@@ -6,12 +6,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
+    def create_user(self, email, username, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -24,13 +23,14 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, username, password, **extra_fields)
+        return self.create_user(email, username, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
     profile_photo = models.ImageField(upload_to='profile/' , null = True , blank= True) 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
